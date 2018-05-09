64954eca717e7ac182c970fb807fd9d7fe844c3e
luyang <980110729@qq.com>
2018-05-09 11:19:31 +0800

    整理静态代码格式

--- YDT/WebRoot/statics/myinvest/duringinvest/js/jquery.cookie.js
+++ YDT/WebRoot/statics/myinvest/duringinvest/js/jquery.cookie.js
@@ -5,18 +5,18 @@
  * Copyright 2013 Klaus Hartl
  * Released under the MIT license
  */
-(function (factory) {
-	if (typeof define === 'function' && define.amd) {
+(function(factory) {
+	if(typeof define === 'function' && define.amd) {
 		// AMD
 		define(['jquery'], factory);
-	} else if (typeof exports === 'object') {
+	} else if(typeof exports === 'object') {
 		// CommonJS
 		factory(require('jquery'));
 	} else {
 		// Browser globals
 		factory(jQuery);
 	}
-}(function ($) {
+}(function($) {
 
 	var pluses = /\+/g;
 
@@ -33,7 +33,7 @@
 	}
 
 	function parseCookieValue(s) {
-		if (s.indexOf('"') === 0) {
+		if(s.indexOf('"') === 0) {
 			// This is a quoted cookie as according to RFC2068, unescape...
 			s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
 		}
@@ -52,24 +52,25 @@
 		return $.isFunction(converter) ? converter(value) : value;
 	}
 
-	var config = $.cookie = function (key, value, options) {
+	var config = $.cookie = function(key, value, options) {
 
 		// Write
 
-		if (value !== undefined && !$.isFunction(value)) {
+		if(value !== undefined && !$.isFunction(value)) {
 			options = $.extend({}, config.defaults, options);
 
-			if (typeof options.expires === 'number') {
-				var days = options.expires, t = options.expires = new Date();
+			if(typeof options.expires === 'number') {
+				var days = options.expires,
+					t = options.expires = new Date();
 				t.setTime(+t + days * 864e+5);
 			}
 
-			return (document.cookie = [
+			return(document.cookie = [
 				encode(key), '=', stringifyCookieValue(value),
 				options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-				options.path    ? '; path=' + options.path : '',
-				options.domain  ? '; domain=' + options.domain : '',
-				options.secure  ? '; secure' : ''
+				options.path ? '; path=' + options.path : '',
+				options.domain ? '; domain=' + options.domain : '',
+				options.secure ? '; secure' : ''
 			].join(''));
 		}
 
@@ -82,19 +83,19 @@
 		// calling $.cookie().
 		var cookies = document.cookie ? document.cookie.split('; ') : [];
 
-		for (var i = 0, l = cookies.length; i < l; i++) {
+		for(var i = 0, l = cookies.length; i < l; i++) {
 			var parts = cookies[i].split('=');
 			var name = decode(parts.shift());
 			var cookie = parts.join('=');
 
-			if (key && key === name) {
+			if(key && key === name) {
 				// If second argument (value) is a function it's a converter...
 				result = read(cookie, value);
 				break;
 			}
 
 			// Prevent storing a cookie that we couldn't decode.
-			if (!key && (cookie = read(cookie)) !== undefined) {
+			if(!key && (cookie = read(cookie)) !== undefined) {
 				result[name] = cookie;
 			}
 		}
@@ -104,13 +105,15 @@
 
 	config.defaults = {};
 
-	$.removeCookie = function (key, options) {
-		if ($.cookie(key) === undefined) {
+	$.removeCookie = function(key, options) {
+		if($.cookie(key) === undefined) {
 			return false;
 		}
 
 		// Must not alter options, thus extending a fresh object...
-		$.cookie(key, '', $.extend({}, options, { expires: -1 }));
+		$.cookie(key, '', $.extend({}, options, {
+			expires: -1
+		}));
 		return !$.cookie(key);
 	};
 
